e64c83e99e679ef59b765214749ffd0ad3a18c5f
Sam Shue <evilrobotfactory@gmail.com>
2019-05-22 16:26:13 -0400

    Odom plotted correctly

--- RosbagPlayback.m
+++ RosbagPlayback.m
@@ -8,17 +8,25 @@ bag = rosbag(filename);
 
 % get odom
 bagSelect = select(bag,'Topic','/odom');
-odomTs = timeseries(bagSelect, 'Pose.Pose.Position.X', 'Pose.Pose.Position.Y', ...
+odomTs = timeseries(bagSelect, 'Pose.Pose.Position.X', 'Pose.Pose.Position.Y', ... %'Twist.Twist.Angular.Z');
     'Pose.Pose.Orientation.X', 'Pose.Pose.Orientation.Y', 'Pose.Pose.Orientation.Z', 'Pose.Pose.Orientation.W');
-odom = odomTs.Data;
+% Convert time seriest to vector
+odom = odomTs.Data; % [x, y, qx, qy, qz, qw] <- odom row contents
 
-orientation = [0, 0, 0];
+
 for ii = 1:length(odom)
     
+    
+    % Plot odom
+    %======================================================================
     hold off;
-    orientation = orientation + quat2eul([odom(ii,6), odom(ii,3), odom(ii,4), odom(ii,5)]);
-    drawRobot(odom(ii,1), odom(ii,2), radtodeg(orientation(3)), 0.25); hold on;
+    % convert quaternions to eulter angels (quat2eul format: WXYZ -> ZXY)
+    orientation = quat2eul([odom(ii,6), odom(ii,3), odom(ii,4), odom(ii,5)]);
+    zOrientation = wrapTo360(rad2deg(orientation(1)));
+    drawRobot(odom(ii,1), odom(ii,2), zOrientation, 0.25); hold on;
     plot(odom(ii,1),odom(ii,2),'o');
     xlim([-5 5]); ylim([-5 5]);
     pause(0.001);
+    % End plot odom
+    %----------------------------------------------------------------------
 end
