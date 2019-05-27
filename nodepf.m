a715affa30aaae8f39df77daf2b1f6850ac1a37c
Sam Shue <evilrobotfactory@gmail.com>
2019-05-27 16:02:53 -0400

    Updated node pf class

--- nodepf.m
+++ nodepf.m
@@ -5,6 +5,9 @@ classdef nodepf
     properties
         numParticles;
         particles;
+        color = 'red';
+        
+        nodeStdDev_m = 1;
     end
     
     methods
@@ -12,14 +15,33 @@ classdef nodepf
             %NODEPF Construct an instance of this class
             %   Detailed explanation goes here
             obj.numParticles = numParticles;
+            
+            particles = zeros(numParticles, 2);
+            % Generate Particles
+            for ii = 1:numParticles
+                orientation = rand() * 360;
+                noise = normrnd(0, nodeStdDev_m);
+                x = cosd(orientation)*(range_m + noise);
+                y = sind(orientation)*(range_m + noise);
+                particles(ii,:) = [x, y];
+            end
+            
+            obj.particles = particles;
         end
         
         function outputArg = predict(obj, motion)
             
         end
         
-        function outputArg = resample(obj, motion)
+        function outputArg = resample(obj, robotPosX, robotPosY, range_m)
             
+        end
+        
+        function plotParticles(obj)
+            hold on;
+            for ii = 1:obj.numParticles
+                scatter(obj.particles(ii,1), obj.particles(ii,2), 'o', obj.color);
+            end
         end
     end
 end
